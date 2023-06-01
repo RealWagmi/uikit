f503a5cecd60f56f6a14aefc51c5403e488ef8a9
belousowork <belousowork@gmail.com>
2023-06-01 14:18:58 +0300

    Updated ButtonOutlined component

--- src/components/Button/ButtonOutlined/index.tsx
+++ src/components/Button/ButtonOutlined/index.tsx
@@ -2,17 +2,17 @@ import styled from "styled-components";
 import { BaseButton } from "../BaseButton";
 
 const ButtonOutlined = styled(BaseButton)`
-  border: 1px solid ${({ theme }) => theme.backgroundOutline};
+  border: 1px solid ${({ theme }) => theme.buttonOutlined.border};
   background-color: transparent;
-  color: ${({ theme }) => theme.textPrimary};
+  color: ${({ theme }) => theme.buttonOutlined.color};
   &:focus {
-    box-shadow: 0 0 0 1px ${({ theme }) => theme.deprecated_bg4};
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.buttonOutlined.focusColor};
   }
   &:hover {
-    box-shadow: 0 0 0 1px ${({ theme }) => theme.textTertiary};
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.buttonOutlined.hoverColor};
   }
   &:active {
-    box-shadow: 0 0 0 1px ${({ theme }) => theme.deprecated_bg4};
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.buttonOutlined.focusColor};
   }
   &:disabled {
     opacity: 50%;
