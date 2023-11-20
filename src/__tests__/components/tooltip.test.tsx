42abde0f8c176960b99a7d0499b06c9806b3fb70
belousovjr <belousowork@gmail.com>
2023-11-20 14:24:44 +0200

    Added inline snapshot test to tooltip component

--- src/__tests__/components/tooltip.test.tsx
+++ src/__tests__/components/tooltip.test.tsx
@@ -6,8 +6,29 @@ import Tooltip from "../../components/Tooltip";
 it("renders correctly", () => {
   const label = "Some Label";
   const content = "Some Content";
-  const { getByText } = renderWithProvider(<Tooltip content={content}>{label}</Tooltip>);
+  const { getByText, asFragment } = renderWithProvider(<Tooltip content={content}>{label}</Tooltip>);
 
   expect(getByText(label)).toBeInTheDocument();
   expect(getByText(content)).toBeInTheDocument();
+
+  expect(asFragment()).toMatchInlineSnapshot(`
+   <DocumentFragment>
+     .c0 {
+     display: -webkit-box;
+     display: -webkit-flex;
+     display: -ms-flexbox;
+     display: flex;
+   }
+   
+   <div
+       class=""
+     >
+       <div
+         class="c0"
+       >
+         Some Label
+       </div>
+     </div>
+   </DocumentFragment>
+  `);
 });
