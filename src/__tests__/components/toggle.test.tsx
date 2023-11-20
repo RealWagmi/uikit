4f9977aa2c44bbde64d87d4958cc0c8d5edf332b
belousovjr <belousowork@gmail.com>
2023-11-20 14:20:37 +0200

    Added inline snapshot test to toggle component

--- src/__tests__/components/toggle.test.tsx
+++ src/__tests__/components/toggle.test.tsx
@@ -7,7 +7,9 @@ import Toggle from "../../components/Toggle";
 describe("Toggle", () => {
   it("should render the toggle and call onChange function for enabled toggle", () => {
     const onChange = vitest.fn();
-    const { getByTestId } = renderWithProvider(<Toggle data-testid={"toggle"} value={true} onChange={onChange} />);
+    const { getByTestId, asFragment } = renderWithProvider(
+      <Toggle data-testid={"toggle"} value={true} onChange={onChange} />
+    );
 
     const toggle = getByTestId("toggle");
 
@@ -17,6 +19,55 @@ describe("Toggle", () => {
 
     expect(onChange).toHaveBeenCalledTimes(1);
     expect(onChange).toHaveBeenCalledWith(false);
+
+    expect(asFragment()).toMatchInlineSnapshot(`
+     <DocumentFragment>
+     .c0 {
+     position: relative;
+     width: 36px;
+     height: 20px;
+     background: #5d93b2;
+     cursor: pointer;
+     border-radius: 20px;
+     padding: 2px;
+     border: none;
+     outline: none;
+     -webkit-transition: background 0.2s;
+     transition: background 0.2s;
+   }
+   
+   .c0:hover:not(:disabled) {
+     background: #6da6c6;
+   }
+   
+   .c0:disabled {
+     background: #6a757b;
+     cursor: default;
+   }
+   
+   .c1 {
+     position: absolute;
+     top: 2px;
+     left: calc(100% - 18px);
+     width: 16px;
+     height: 16px;
+     background: #ffffff;
+     border-radius: inherit;
+     -webkit-transition: left 0.2s,background 0.2s;
+     transition: left 0.2s,background 0.2s;
+   }
+   
+   <button
+       class="c0"
+       data-testid="toggle"
+       type="button"
+     >
+       <span
+         class="c1"
+       />
+     </button>
+   </DocumentFragment>
+    `);
   });
   it("should not call onChange function for disabled toggle", () => {
     const onChange = vitest.fn();
