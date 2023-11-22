104b4674dcb7b680186ba07752c4251eb4d7f4ab
belousovjr <belousowork@gmail.com>
2023-11-22 12:42:24 +0200

    Added some icons

--- src/components/Svg/Icons/index.tsx
+++ src/components/Svg/Icons/index.tsx
@@ -3,3 +3,6 @@ export { default as QuestionIcon } from "./components/QuestionIcon";
 export { default as InfoIcon } from "./components/InfoIcon";
 export { default as ArrowDownIcon } from "./components/ArrowDownIcon";
 export { default as SortIcon } from "./components/SortIcon";
+export { default as SettingsIcon } from "./components/SettingsIcon";
+export { default as WarningIcon } from "./components/WarningIcon";
+export { default as ClearIcon } from "./components/ClearIcon";
