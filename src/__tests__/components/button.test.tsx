1db4eebb2cb9bc7d7f234dfefc51b5230263fdc4
belousovjr <belousowork@gmail.com>
2023-11-28 15:27:34 +0200

    Updated button test

--- src/__tests__/components/button.test.tsx
+++ src/__tests__/components/button.test.tsx
@@ -16,135 +16,103 @@ describe("Button", () => {
     const buttonElement = getByText(label);
     expect(buttonElement).toBeInTheDocument();
     expect(asFragment()).toMatchInlineSnapshot(`
-    <DocumentFragment>
-    .c4 {
-    height: 16px;
-    width: 16px;
-    stroke: #5d93b2;
-  }
-
-  .c5 {
-    fill: none !important;
-    -webkit-animation: 0.4s fvtopB linear infinite;
-    animation: 0.4s fvtopB linear infinite;
-  }
-
-  .c0 {
-    width: 100%;
-    background: linear-gradient(90deg,#7da0c1 3.46%,#34627d 100%);
-    outline: none;
-    color: #f7f8f2;
-    box-shadow: 0 5px 13px -4px rgba(169,182,191,0.08);
-    padding: 12px 16px;
-    font-size: 16px;
-    border-radius: 16px;
-    position: relative;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-pack: center;
-    -webkit-justify-content: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    -webkit-align-items: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    box-sizing: border-box;
-    border: none;
-    cursor: pointer;
-    -webkit-text-decoration: none;
-    text-decoration: none;
-  }
-
-  .c0:hover:not(:disabled),
-  .c0:focus {
-    background: linear-gradient(90deg,#91B9de 3.46%,#40708b 100%);
-  }
-
-  .c0 svg {
-    fill: #f7f8f2;
-    stroke: #f7f8f2;
-  }
-
-  .c0:disabled {
-    box-shadow: none;
-  }
-
-  .c0:disabled {
-    cursor: default;
-    opacity: 0.5;
-  }
-
-  .c1 {
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-align-items: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    -webkit-justify-content: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    opacity: 1;
-    -webkit-transition: opacity 0.15s;
-    transition: opacity 0.15s;
-  }
-
-  .c3 {
-    position: absolute;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    opacity: 0;
-    pointer-events: none;
-    -webkit-transition: opacity 0.15s;
-    transition: opacity 0.15s;
-  }
-
-  .c2 {
-    margin: 0 8px;
-  }
-
-  <button
-      class="c0"
-      color="primary"
-      scale="default"
-      width="100%"
-    >
-      <span
-        class="c1"
-      >
-        <span
-          class="c2"
-        >
-          ${label}
-        </span>
-      </span>
-      <span
-        class="c3"
-      >
-        <svg
-          class="c4 c5"
-          fill="none"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 9.27455 20.9097 6.80375"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2.5"
-          />
-        </svg>
-      </span>
-    </button>
-  </DocumentFragment>
+   <DocumentFragment>
+     .c0 {
+     width: 100%;
+     background: linear-gradient(90deg,#7da0c1 3.46%,#34627d 100%);
+     outline: none;
+     color: #f7f8f2;
+     box-shadow: 0 5px 13px -4px rgba(169,182,191,0.08);
+     padding: 12px 16px;
+     font-size: 16px;
+     border-radius: 16px;
+     display: -webkit-box;
+     display: -webkit-flex;
+     display: -ms-flexbox;
+     display: flex;
+     -webkit-box-pack: center;
+     -webkit-justify-content: center;
+     -ms-flex-pack: center;
+     justify-content: center;
+     -webkit-align-items: center;
+     -webkit-box-align: center;
+     -ms-flex-align: center;
+     align-items: center;
+     box-sizing: border-box;
+     border: none;
+     cursor: pointer;
+     -webkit-text-decoration: none;
+     text-decoration: none;
+   }
+   
+   .c0:hover:not(:disabled),
+   .c0:focus {
+     background: linear-gradient(90deg,#91B9de 3.46%,#40708b 100%);
+   }
+   
+   .c0 svg {
+     fill: #f7f8f2;
+     stroke: #f7f8f2;
+   }
+   
+   .c0:disabled {
+     box-shadow: none;
+   }
+   
+   .c0:disabled {
+     cursor: default;
+     opacity: 0.5;
+   }
+   
+   .c1 {
+     display: -webkit-box;
+     display: -webkit-flex;
+     display: -ms-flexbox;
+     display: flex;
+     -webkit-align-items: center;
+     -webkit-box-align: center;
+     -ms-flex-align: center;
+     align-items: center;
+     -webkit-box-pack: center;
+     -webkit-justify-content: center;
+     -ms-flex-pack: center;
+     justify-content: center;
+     -webkit-transition: opacity 0.15s;
+     transition: opacity 0.15s;
+   }
+   
+   .c2 {
+     position: relative;
+     margin: 0 12px;
+   }
+   
+   .c2:after {
+     display: none;
+     content: ".";
+     position: absolute;
+     left: 100%;
+     bottom: 0;
+     -webkit-animation: hfLTkO 0.6s infinite;
+     animation: hfLTkO 0.6s infinite;
+   }
+   
+   <button
+       class="c0"
+       color="primary"
+       scale="default"
+       width="100%"
+     >
+       <span
+         class="c1"
+       >
+         <span
+           class="c2"
+         >
+           Some Button
+         </span>
+       </span>
+     </button>
+   </DocumentFragment>
     `);
   });
 
