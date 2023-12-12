19d6652e762b9f124e86b9797d1006e703f83573
belousovjr <belousowork@gmail.com>
2023-12-12 14:47:34 +0200

    Updated plus icon stroke width

--- src/components/Svg/Icons/components/PlusIcon.tsx
+++ src/components/Svg/Icons/components/PlusIcon.tsx
@@ -4,7 +4,7 @@ import { SvgProps } from "../../types";
 
 const Icon: React.FC<React.PropsWithChildren<SvgProps>> = (props) => (
   <Svg viewBox="0 0 24 24" {...props} fill="none">
-    <path d="M3.75 12H20.25M12 3.75V20.25" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M3.75 12H20.25M12 3.75V20.25" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5px" />
   </Svg>
 );
 
