5c85e96cbd5fd79bce8edbcd2a08129620bd706b
belousovjr <belousowork@gmail.com>
2023-12-28 16:36:25 +0200

    Fixed IRowProps type

--- src/components/Box/components/Row/types.ts
+++ src/components/Box/components/Row/types.ts
@@ -4,7 +4,7 @@ import { CSSProperties } from "react";
 export interface IRowProps extends Omit<BoxProps, "display"> {
   width?: string;
   align?: string;
-  justify?: CSSProperties["justifyContent"];
+  justify?: 'start' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
   padding?: string;
   border?: string;
   borderRadius?: string;
