b9df77221e088fffdd7552a919954e96d2928cd7
belousovjr <belousowork@gmail.com>
2023-12-28 16:25:30 +0200

    Updated some types of row and svg

--- src/components/Box/components/Row/types.ts
+++ src/components/Box/components/Row/types.ts
@@ -1,9 +1,10 @@
 import { BoxProps } from "../../types";
+import { CSSProperties } from "react";
 
-export interface IRowProps extends Omit<BoxProps, 'display'> {
+export interface IRowProps extends Omit<BoxProps, "display"> {
   width?: string;
   align?: string;
-  justify?: 'start' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+  justify?: CSSProperties["justifyContent"];
   padding?: string;
   border?: string;
   borderRadius?: string;
