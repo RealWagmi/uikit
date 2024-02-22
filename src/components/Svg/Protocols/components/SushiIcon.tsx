5d26f69747a4897e1ef9c29302f64e92dafe2e45
belousovjr <belousowork@gmail.com>
2024-02-22 21:23:31 +0200

    Updated protocols icons

--- src/components/Svg/Protocols/components/SushiIcon.tsx
+++ src/components/Svg/Protocols/components/SushiIcon.tsx
@@ -9,24 +9,23 @@ const Item: FC<PropsWithChildren<SvgProps>> = (props): ReactElement => {
   const clipId03 = uuid();
 
   const gradId01 = uuid();
-
   const gradId02 = uuid();
 
   return (
-    <Svg viewBox="0 0 18 18" {...props} fill="none" stroke="none">
+    <Svg viewBox="0 0 24 24" {...props} fill="none" stroke="none">
       <g clipPath={`url(#${clipId01})`}>
         <g clipPath={`url(#${clipId02})`}>
-          <circle cx="8.89" cy="8.89" r="8.89" fill={`url(#${gradId01})`} />
-          <circle cx="8.89" cy="8.89" r="8.39" stroke="white" strokeOpacity="0.2" />
+          <circle cx="11.8516" cy="11.8518" r="12" fill={`url(#${gradId01})`} />
+          <circle cx="11.8516" cy="11.8518" r="11.5" stroke="white" strokeOpacity="0.2" />
           <g clipPath={`url(#${clipId03})`}>
             <path
-              d="M13.832 10.252L11.908 13.092L11.892 13.08C11.2 14.008 8.96803 13.624 6.86003 12.132C4.76403 10.652 3.55603 8.768 4.15603 7.752L4.14003 7.74L6.06403 4.944C6.73203 3.984 8.98803 4.356 11.116 5.864C13.288 7.284 14.46 9.288 13.832 10.252Z"
+              d="M18.7648 13.7531L16.0712 17.7291L16.0488 17.7123C15.08 19.0115 11.9552 18.4739 9.00401 16.3851C6.06961 14.3131 4.37842 11.6755 5.21841 10.2531L5.19601 10.2363L7.88961 6.3219C8.82481 4.9779 11.9832 5.4987 14.9624 7.60989C18.0032 9.59789 19.644 12.4035 18.7648 13.7531Z"
               fill={`url(#${gradId02})`}
             />
             <path
               fillRule="evenodd"
               clipRule="evenodd"
-              d="M13.5401 10.0881C12.9521 10.9241 10.9081 10.5041 8.94413 9.12807C6.94013 7.74807 5.76813 5.99607 6.35213 5.16007C6.93613 4.32407 8.98413 4.74407 10.9481 6.12007C12.9081 7.50007 14.0801 9.25207 13.5401 10.0881ZM11.7001 8.83207C11.4081 9.24807 10.4041 9.04407 9.40013 8.37207C8.44013 7.70407 7.85613 6.82807 8.14813 6.41207C8.44013 5.99607 9.44413 6.20007 10.4481 6.87207C11.4081 7.54007 11.9921 8.41607 11.7001 8.83207Z"
+              d="M18.356 13.5234C17.5328 14.6938 14.6712 14.1058 11.9216 12.1794C9.11603 10.2474 7.47523 7.79459 8.29283 6.62419C9.11043 5.45379 11.9776 6.04179 14.7272 7.96819C17.4712 9.90019 19.112 12.353 18.356 13.5234ZM15.78 11.765C15.3712 12.3474 13.9656 12.0618 12.56 11.121C11.216 10.1858 10.3984 8.95939 10.8072 8.37699C11.216 7.79459 12.6216 8.08019 14.0272 9.02099C15.3712 9.95618 16.1888 11.1826 15.78 11.765Z"
               fill="white"
             />
           </g>
@@ -39,17 +38,17 @@ const Item: FC<PropsWithChildren<SvgProps>> = (props): ReactElement => {
           cy="0"
           r="1"
           gradientUnits="userSpaceOnUse"
-          gradientTransform="translate(8.89 8.89) rotate(90) scale(8.89)"
+          gradientTransform="translate(11.8516 11.8518) rotate(90) scale(12)"
         >
           <stop stopColor="#FFDBEC" />
           <stop offset="1" stopColor="#8089C9" />
         </radialGradient>
         <linearGradient
           id={gradId02}
-          x1="7.98543"
-          y1="6.43386"
-          x2="10.0254"
-          y2="11.5619"
+          x1="10.5796"
+          y1="8.40769"
+          x2="13.4356"
+          y2="15.5869"
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#29AFE5" />
@@ -57,13 +56,13 @@ const Item: FC<PropsWithChildren<SvgProps>> = (props): ReactElement => {
           <stop offset="1" stopColor="#FA52A0" />
         </linearGradient>
         <clipPath id={clipId01}>
-          <rect x="0.109863" y="0.109863" width="17.78" height="17.78" rx="8.89" fill="white" />
+          <rect width="24" height="24" rx="12" fill="white" />
         </clipPath>
         <clipPath id={clipId02}>
-          <rect width="17.78" height="17.78" rx="8.89" fill="white" />
+          <rect x="-0.148438" y="-0.148193" width="24" height="24" rx="10" fill="white" />
         </clipPath>
         <clipPath id={clipId03}>
-          <rect width="10" height="10" fill="white" transform="translate(4 4)" />
+          <rect width="14" height="14" fill="white" transform="translate(5 5.00024)" />
         </clipPath>
       </defs>
     </Svg>
