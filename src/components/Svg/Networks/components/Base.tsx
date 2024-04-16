a8f672cc0e33f10643f0c7aba07ba79f4a618347
Alex <tarkovskiy.alexandr@protonmail.com>
2024-04-16 18:44:38 +0300

    update icons

--- src/components/Svg/Networks/components/Base.tsx
+++ src/components/Svg/Networks/components/Base.tsx
@@ -5,10 +5,10 @@ import { SvgProps } from "../../types";
 const Item: FC<PropsWithChildren<SvgProps>> = (props): ReactElement => {
   return (
     <Svg viewBox="0 0 32 32" {...props} fill="none" stroke="none">
-        <g clip-path="url(#clip0_11876_39199)">
+        <g clipPath="url(#clip0_11876_39199)">
             <rect width="32" height="32" rx="6" fill="url(#paint0_radial_11876_39199)"/>
-            <rect x="0.5" y="0.5" width="31" height="31" rx="5.5" stroke="white" stroke-opacity="0.2"/>
-            <g clip-path="url(#clip1_11876_39199)">
+            <rect x="0.5" y="0.5" width="31" height="31" rx="5.5" stroke="white" strokeOpacity="0.2"/>
+            <g clipPath="url(#clip1_11876_39199)">
                 <path d="M15.8957 25.8259C21.38 25.8259 25.8259 21.3877 25.8259 15.913C25.8259 10.4382 21.38 6 15.8957 6C10.6925 6 6.42393 9.99485 6 15.0797H19.1255V16.7462H6C6.42393 21.8311 10.6925 25.8259 15.8957 25.8259Z" fill="white"/>
             </g>
         </g>
--- src/components/Svg/Wallets/components/WalletConnect.tsx
+++ src/components/Svg/Wallets/components/WalletConnect.tsx
@@ -7,7 +7,7 @@ const Item: FC<PropsWithChildren<SvgProps>> = (props): ReactElement => {
   return (
     <Svg viewBox="0 0 32 32" {...props} fill="none" stroke="none">
       <rect width="32" height="32" rx="8" fill="#0052FF"/>
-      <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" stroke="white" stroke-opacity="0.2"/>
+      <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" stroke="white" strokeOpacity="0.2"/>
       <path d="M16.2531 21.875C13.1469 21.875 10.6281 19.3594 10.6281 16.25C10.6281 13.1406 13.1469 10.625 16.2531 10.625C19.0375 10.625 21.35 12.6562 21.7938 15.3125H27.4594C26.9813 9.5375 22.1469 5 16.25 5C10.0375 5 5 10.0375 5 16.25C5 22.4625 10.0375 27.5 16.25 27.5C22.1469 27.5 26.9813 22.9625 27.4594 17.1875H21.7906C21.3438 19.8438 19.0375 21.875 16.2531 21.875Z" fill="white"/>
     </Svg>
   );
